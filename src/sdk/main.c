29326c64543c48f610f477e7da4311fe1ef9fd31
UEC2-Pong <uec2.pongproject@gmail.com>
2020-09-07 19:17:43 +0200

    changes to main.c

--- src/sdk/main.c
+++ src/sdk/main.c
@@ -1,3 +1,44 @@
+//SYSTEM SPECIFICATION
+/*In order to control the screen and the game itself two 32bit gpio ports are used.
+ *	gpio 1: chooses active screen, displays highlight on icons, shows brackets on currently selected
+ *	game speed and controls ball movement
+ *	gpio 2: is in control of palette movement
+ *
+ *	Control bit distribution is as follows (from MSB to LSB):
+ *	gpio1:
+ *	31-30: 2 bits for screen selection
+ *	29-28: 2 bits for icon highlight
+ *	27-26: 2 bits for game speed selection
+ *	25-22: 4 unused bits
+ *	21-11: 11 bits for ball Y position
+ *	10-0 : 11 bits for ball X position.
+ *
+ *	gpio2:
+ *	31-22: 10 unused bits
+ *	21-11: 11 bits for left palette position
+ *	10-0 : 11 bits for right palette position
+ *
+ *	Unless in Game screen changes to palette and ball bits are not visible, but might determine starting
+ *	positions for both players and the ball.
+ *	GAME SPEED IS SELECTED VIA SOFTWARE. Game speed bits sent via gpio1 ONLY show brackets in Options screen.
+ *	These should correspond with game speed selected in software.
+ *
+ *	Screen selection codes:
+ *	0 - Menu
+ *	1 - Game (both single and multiplayer, differences between the two are software based)
+ *	2 - Credits
+ *	3 - Options
+ *
+ *	Icon highlighter codes: TOP icon has code 0. Increment to reach each icon downward.
+ *
+ *	Game speed codes:
+ *	0 - Fast
+ *	1 - Medium
+ *	2 - Slow
+ *	3 - Possible to send, but unused
+ *
+ *	Screen resolution is 1024x768
+ */
 // includes
 #include "xil_types.h"
 #include "xgpio.h"
@@ -6,199 +47,219 @@
 #include "xil_printf.h"
 #include "sleep.h"
 
-uint8_t screen_mode		 = 0x00;
-uint8_t icon_highlighter = 0x00;
-uint8_t game_speed 		 = 0x01;
-uint8_t game_mode        = 0x00;
+//------------------------------------------------------------------------------------------------------------------------------------
+//DEFINES
+#define FAST 	5
+#define MEDIUM 	3
+#define SLOW 	1
+//------------------------------------------------------------------------------------------------------------------------------------
+//GLOBAL VARIABLES
+
+XGpio gpio;
 uint32_t gpio_output, gpio2_output;
-uint8_t KeyStatus;
+enum ScreenMode {MENU, GAME, CREDITS, OPTIONS} eScreenMode = MENU;		//MENU   set as initial screen
 
-void Menu(void);
-void Options(void);
-void SingleGame(void);
-void MultiGame(void);
+enum GameMode 	{SINGLEPLAYER, MULTIPLAYER};
+enum KeyStatus  {RELEASED, W, S, ARROW_UP, ARROW_DOWN, ENTER, ESC};
 
-void CheckingForKeys(uint32_t baseaddr) {
-	if(!XUartLite_IsReceiveEmpty(baseaddr)){
-		uint8_t ReceivedKey;
-		ReceivedKey = XUartLite_RecvByte(baseaddr);
-		if 		(ReceivedKey == 'w') KeyStatus = 1; // W
-		else if (ReceivedKey == 's') KeyStatus = 2; // S
-		else if (ReceivedKey == 13)  KeyStatus = 3; // Enter
-		else if (ReceivedKey == 27)  KeyStatus = 4; // Esc
-		else if (ReceivedKey == 38)  KeyStatus = 5; // Arrow Up
-		else if (ReceivedKey == 40)  KeyStatus = 6; // Arrow Down
-		else KeyStatus = 0;
-	}
-	else{
-		KeyStatus = 0;
+//------------------------------------------------------------------------------------------------------------------------------------
+//FUNCTION DECLARATIONS (TO BE SENT TO LIBRARIES)
+
+void Init(void);
+void Game(enum GameMode, uint8_t);
+void SendGame(uint16_t, uint16_t, uint16_t, uint16_t);
+void Credits(void);
+void Options(uint8_t *);
+void SendOptions(uint8_t, uint8_t);
+void SendCredits(void);
+void SendMenu(uint8_t);
+enum KeyStatus CheckingForKeys(uint32_t);
+
+//------------------------------------------------------------------------------------------------------------------------------------
+// MAIN LOOP
+
+int main(){
+
+	uint8_t icon_highlighter = 0x00;
+	uint8_t GameSpeed 	  = MEDIUM;
+	enum KeyStatus eKeyStatus;
+	Init();
+
+	while(1){
+		eKeyStatus = CheckingForKeys(XPAR_AXI_UARTLITE_0_BASEADDR);
+		if(eKeyStatus == W) {
+			icon_highlighter = (icon_highlighter+3)%4;
+			xil_printf("pressed: 'w' ");
+		}
+		else if(eKeyStatus == S) {
+			icon_highlighter = (icon_highlighter+1)%4;
+			xil_printf("pressed: 's' ");
+		}
+		else if(eKeyStatus == ENTER){
+			xil_printf("pressed: 'enter' ");
+			if	   ( icon_highlighter == 0 ) {
+				eScreenMode = GAME;
+				Game(SINGLEPLAYER, GameSpeed);
+			}
+			else if( icon_highlighter == 1 ) {
+				eScreenMode = GAME;
+				Game(MULTIPLAYER, GameSpeed);
+			}
+			else if( icon_highlighter == 2 ) {
+				eScreenMode = CREDITS;
+				Credits();
+			}
+			else if( icon_highlighter == 3 ) {
+				eScreenMode = OPTIONS;
+				Options(&GameSpeed);
+			}
+			else xil_printf("ERROR ");
+		}
+		else if(eKeyStatus == ESC){
+			xil_printf("pressed: 'esc' ");
+		}
+		else{
+		}
+
+		SendMenu(icon_highlighter);
+		usleep(2500);
 	}
 }
 
-// MAIN LOOP
 //------------------------------------------------------------------------------------------------------------------------------------
+//FUNCTION DEFINITIONS (TO BE SENT TO LIBRARIES)
 
-int main(){
-	XGpio gpio;
+enum KeyStatus CheckingForKeys(uint32_t baseaddr) {
+	if(!XUartLite_IsReceiveEmpty(baseaddr)){
+		uint8_t ReceivedKey;
+		ReceivedKey = XUartLite_RecvByte(baseaddr);
+		if 		(ReceivedKey == 'w') return W;
+		else if (ReceivedKey == 's') return S;
+		else if (ReceivedKey == 38)  return ARROW_UP;
+		else if (ReceivedKey == 40)  return ARROW_DOWN;
+		else if (ReceivedKey == 13)  return ENTER;
+		else if (ReceivedKey == 27)  return ESC;
+		else return RELEASED;
+	}
+	else{
+		return RELEASED;
+	}
+}
+
+void Init(void){
 	XGpio_Initialize(&gpio, 0);
 	gpio_output  = 0x00000000;
 	gpio2_output = 0x000C0180;
 	XGpio_DiscreteWrite(&gpio, 1, gpio_output);
 	XGpio_DiscreteWrite(&gpio, 2, gpio2_output);
+}
 
-	while(1){
-		CheckingForKeys(XPAR_AXI_UARTLITE_0_BASEADDR);
-		if( screen_mode == 0){
-			Menu();
-		}
-		else if( screen_mode == 1){
-			SingleGame();
-			if(KeyStatus == 4){
-				screen_mode 	 = 0;
-				game_mode		 = 0;
-				icon_highlighter = 0;
-				xil_printf("pressed: 'esc' ");
-			}
-			else {}
-		}
-		else if( screen_mode == 2){
-			if(KeyStatus == 4){
-				screen_mode  	 = 0;
-				icon_highlighter = 2;
-				xil_printf("pressed: 'esc' ");
-			}
-			else {}
-		}
-		else if( screen_mode == 3){
-			Options();
-		}
-		else{
-			xil_printf("you shouldn't be here ");
-		}
-		gpio_output = screen_mode<<30;
-		gpio_output = gpio_output | (icon_highlighter<<28);
-		gpio_output = gpio_output | (game_speed<<26);
-		XGpio_DiscreteWrite(&gpio, 1, gpio_output);
+void SendMenu(uint8_t icon_highlighter){
+	gpio_output = eScreenMode<<30;
+	gpio_output = gpio_output | (icon_highlighter<<28);
 
-		/*
-		gpio2_output = screen_mode<<30;
-		gpio2_output = gpio2_output | (icon_highlighter<<28);
-		gpio2_output = gpio2_output | (game_speed<<26);
-		XGpio_DiscreteWrite(&gpio, 2, gpio2_output);
-		*/
+	XGpio_DiscreteWrite(&gpio, 1, gpio_output);
+}
+
+
+void Game(enum GameMode eGameMode, uint8_t GameSpeed){
+	enum KeyStatus eKeyStatus;
+	uint16_t left_palette  = 0x180;
+	uint16_t right_palette = 0x180;
+	uint16_t ball_xpos	   = 0x200;
+	uint16_t ball_ypos	   = 0x180;
+
+	while(eScreenMode == GAME){
+		eKeyStatus = CheckingForKeys(XPAR_AXI_UARTLITE_0_BASEADDR);
+		if(eKeyStatus == ESC){
+			eScreenMode 	 = MENU;
+			xil_printf("pressed: 'esc' ");
+		}
+		else if(eKeyStatus == W) {
+			left_palette = left_palette - GameSpeed;
+			right_palette = right_palette - GameSpeed;
+			xil_printf("pressed: 'w' ");
+		}
+		else if(eKeyStatus == S) {
+			left_palette = left_palette + GameSpeed;
+			right_palette = right_palette + GameSpeed;
+			xil_printf("pressed: 'w' ");
+		}
+		else {}
+		SendGame(left_palette, right_palette, ball_xpos, ball_ypos);
 	}
+}
+
+void SendGame(uint16_t left_palette, uint16_t right_palette, uint16_t ball_xpos, uint16_t ball_ypos){
+	gpio_output = eScreenMode<<30;
+	gpio_output = gpio_output | (ball_ypos<<11) | ball_xpos;
+
+	gpio2_output = right_palette;
+	gpio2_output = gpio2_output | (left_palette << 11);
+
+	XGpio_DiscreteWrite(&gpio, 1, gpio_output);
+	XGpio_DiscreteWrite(&gpio, 2, gpio2_output);
+}
+
+void Credits(void){
+	enum KeyStatus eKeyStatus;
+
+	while( eScreenMode == CREDITS){
+		eKeyStatus = CheckingForKeys(XPAR_AXI_UARTLITE_0_BASEADDR);
+		if(eKeyStatus == ESC){
+			eScreenMode  	 = MENU;
+			xil_printf("pressed: 'esc' ");
+		}
+		else {}
+	SendCredits();
+	usleep(2500);
+	}
+}
+
+void SendCredits(void){
+	gpio_output = eScreenMode<<30;
+	XGpio_DiscreteWrite(&gpio, 1, gpio_output);
+}
+
+void Options(uint8_t *GameSpeed){
+	enum KeyStatus eKeyStatus;
+	uint8_t aSpeedValue[3] = {FAST, MEDIUM, SLOW};
+	uint8_t icon_highlighter = 0x01;
+	static uint8_t GameSpeedCode = 1;
+
+	while(eScreenMode == OPTIONS){
+		eKeyStatus = CheckingForKeys(XPAR_AXI_UARTLITE_0_BASEADDR);
+		if(eKeyStatus == W) {
+			icon_highlighter = (icon_highlighter+2)%3;
+			xil_printf("pressed: 'w' ");
+		}
+		else if(eKeyStatus == S) {
+			icon_highlighter = (icon_highlighter+1)%3;
+			xil_printf("pressed: 's' ");
+		}
+		else if(eKeyStatus == ENTER){
+			GameSpeedCode = icon_highlighter;
+			*GameSpeed 	  = aSpeedValue[icon_highlighter];
+			xil_printf("pressed: 'enter' ");
+		}
+		if(eKeyStatus == ESC){
+			eScreenMode 	 = MENU;
+			icon_highlighter = 3;
+			xil_printf("pressed: 'esc' ");
+		}
+		else {}
+		SendOptions(GameSpeedCode, icon_highlighter);
+		usleep(2500);
+	}
+}
+
+void SendOptions(uint8_t GameSpeedCode, uint8_t icon_highlighter){
+	gpio_output = eScreenMode<<30;
+	gpio_output = gpio_output | (icon_highlighter<<28);
+	gpio_output = gpio_output | (GameSpeedCode<<26);
+	XGpio_DiscreteWrite(&gpio, 1, gpio_output);
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------
+//ARCHIVES
 
-void Menu(void){
-	if(KeyStatus == 1) {
-		icon_highlighter = (icon_highlighter+3)%4;
-		xil_printf("pressed: 'w' ");
-	}
-	else if(KeyStatus == 2) {
-		icon_highlighter = (icon_highlighter+1)%4;
-		xil_printf("pressed: 's' ");
-	}
-	else if(KeyStatus == 3){
-		xil_printf("pressed: 'enter' ");
-		if( icon_highlighter == 0 ){
-			screen_mode = 1;
-			game_mode   = 1;
-		}
-		else if( icon_highlighter == 1 ){
-			screen_mode = 1;
-			game_mode   = 2;
-		}
-		else if( icon_highlighter == 2 ) screen_mode = 2;
-		else if( icon_highlighter == 3 ) screen_mode = 3;
-		else {
-			xil_printf("ERROR ");
-		}
-		icon_highlighter = 0;
-	}
-	else if(KeyStatus == 4){
-		xil_printf("pressed: 'esc' ");
-	}
-	else{
-	}
-}
-
-void Options(void){
-	if(KeyStatus == 1) {
-		icon_highlighter = (icon_highlighter+2)%3;
-		xil_printf("pressed: 'w' ");
-	}
-	else if(KeyStatus == 2) {
-		icon_highlighter = (icon_highlighter+1)%3;
-		xil_printf("pressed: 's' ");
-	}
-	else if(KeyStatus == 3){
-		game_speed = icon_highlighter;
-		xil_printf("pressed: 'enter' ");
-	}
-	if(KeyStatus == 4){
-		screen_mode 	 = 0;
-		icon_highlighter = 3;
-		xil_printf("pressed: 'esc' ");
-	}
-	else {}
-}
-
-void SingleGame(void){
-	if(KeyStatus == 1){
-		gpio_output--;
-		xil_printf("pressed: 'w' ");
-	}
-	else if(KeyStatus == 2){
-		gpio_output++;
-		xil_printf("pressed: 's' ");
-	}
-}
-
-void MultiGame(void){
-	if(KeyStatus == 1){
-		gpio_output--;
-		xil_printf("pressed: 'w' ");
-	}
-	else if(KeyStatus == 2){
-		gpio_output++;
-		xil_printf("pressed: 's' ");
-	}
-	else if(KeyStatus == 5){
-		gpio2_output--;
-		xil_printf("pressed: 'Arrow Up' ");
-	}
-	else if(KeyStatus == 6){
-		gpio2_output++;
-		xil_printf("pressed: 'Arrow Down; ");
-	}
-}
-
-/*
-	while(1){
-		CheckingForKeys(XPAR_AXI_UARTLITE_0_BASEADDR);
-		if(KeyStatus == 1) {
-			screen_mode = 0b00;
-			xil_printf("pressed: 'w' ");
-		}
-		else if(KeyStatus == 2) {
-			screen_mode = 0b01;
-			xil_printf("pressed: 's' ");
-		}
-		else if(KeyStatus == 3){
-			screen_mode = 0b11;
-			xil_printf("pressed: 'enter' ");
-		}
-		else if(KeyStatus == 4){
-			screen_mode = 0b10;
-			xil_printf("pressed: 'esc' ");
-		}
-		else{
-		}
-	gpio_output = screen_mode<<30;
-	XGpio_DiscreteWrite(&gpio, 1, gpio_output);
-	XGpio_DiscreteWrite(&gpio, 2, gpio2_output);
-	usleep(2500);
-	}
-	*/
